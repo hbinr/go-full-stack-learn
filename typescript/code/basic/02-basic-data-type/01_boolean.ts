99343a325c85f545dbe65773302c02e8ebefe8ba
hbinr <1807227469@qq.com>
2021-05-13 21:30:20 +0800

    update:typescript 调整boolean|tuple笔记

--- typescript/code/basic/02-basic-data-type/01_boolean.ts
+++ typescript/code/basic/02-basic-data-type/01_boolean.ts
@@ -2,6 +2,8 @@
  * 布尔类型学习，内容要点：
  * 1. 定义语法  let ok: boolean = true
  * 2. 关键字为 boolean，在Go中为bool
+ * 3. 类型注解，相当于强类型语言中类型声明
+ * 4. 类型注解语法： (变量/函数): 数据类型  let ok: boolean = true ,  function fn(): res{}
  */
 
  let ok: boolean = true
