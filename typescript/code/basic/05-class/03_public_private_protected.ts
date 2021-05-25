44ebe274050bc7f4c10b566672fca0b4cd024116
hbinr <dhb1807227469@.163.com>
2021-05-25 22:22:02 +0800

    update: typescript class笔记修改

--- typescript/code/basic/05-class/03_public_private_protected.ts
+++ typescript/code/basic/05-class/03_public_private_protected.ts
@@ -5,18 +5,19 @@
  * 3. public 公有的，ts中默认修饰符，可省略不写。类内、类外都可以访问
  * 4. private 私有的，只能类自身内部访问，类外部 和 子类 都不允许访问
  * 5. protected 受保护的，只能在类自身内部及其子类中访问，外部不允许访问
- * 6. Go中并没有类的概念，而是结构体 struct
+ * 6. 一句话总结：public哪都能用，private只有基类可以用，protected基类和派生类中可以用
+ * 7. Go中并没有类的概念，而是结构体 struct
  *     
  */
 
 // 1. 私有属性定义
-class Animal2{
+class Animal2 {
     private name: string
 
-    constructor(name: string){
+    constructor(name: string) {
         this.name = name
     }
-    private eat(){}
+    private eat() { }
 }
 
 let animal = new Animal2('animal')
@@ -24,18 +25,18 @@ let animal = new Animal2('animal')
 // animal.name   // 报错：属性“name”为私有属性，只能在类“Animal2”中访问。
 // animal.eat()  // 报错：属性“eat”为私有属性，只能在类“Animal2”中访问。
 
-class Dog2 extends Animal2{
+class Dog2 extends Animal2 {
     color: string
-    constructor(name:string, color: string){
+    constructor(name: string, color: string) {
         super(name)
         this.color = color
     }
-    printParentName(){
+    printParentName() {
         // 子类不能访问父类的私有属性
         // console.log('this.name: ', this.name);   // 报错：属性“name”为私有属性，只能在类“Animal2”中访问。
     }
 }
-let dog = new Dog2('阿黄','yellow')
+let dog = new Dog2('阿黄', 'yellow')
 dog.color = 'black'
 // 类外部不能访问 私有属性，即使是子类也不行
 // dog.name   // 报错：属性“name”为私有属性，只能在类“Animal2”中访问。
@@ -44,13 +45,13 @@ dog.color = 'black'
 
 
 // 2. 受保护属性定义
-class Cat{
+class Cat {
     protected name: string
 
-    constructor(name: string){
+    constructor(name: string) {
         this.name = name
     }
-    protected eat(){}
+    protected eat() { }
 }
 
 let cat = new Cat('cat')
@@ -61,7 +62,7 @@ class PersianCat extends Cat {
     constructor(name: string) {
         super(name)
     }
-    printInfo(){
+    printInfo() {
         // 子类内部是允许访问的
         this.name = '波斯猫' //此处的 name 就是 Cat 类中的 name
         this.eat()  //此处的 ear() 就是 Cat 类中的 eat()
